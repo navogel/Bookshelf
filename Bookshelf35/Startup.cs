bf7ed6c6eb76c93447f3c64434b7ae4a9d7f33a8
Nate V <navogel@gmail.com>
2020-01-23 15:47:57 -0600

    added many to many model genre and bookgenre

--- Bookshelf35/Startup.cs
+++ Bookshelf35/Startup.cs
@@ -28,7 +28,7 @@ namespace Bookshelf35
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<DbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<ApplicationUser>()
